804a07588e2f1953dc44d32c0ae6196a70f72ad1
krishanthasamaraweera <krishantha@wso2.com>
2014-03-03 17:04:02 +0530

    remove new test framework-2

--- test-automation-framework-2/org.wso2.carbon.automation.engine/src/main/java/org/wso2/carbon/automation/engine/context/HostType.java
+++
@@ -1,7 +0,0 @@
-package org.wso2.carbon.automation.engine.context;
-
-
-public enum HostType {
-    manager,
-    worker
-}
--- test-automation-framework-2/org.wso2.carbon.automation.extensions/src/main/java/org/wso2/carbon/automation/extensions/servers/tomcatserver/TomcatServerType.java
+++
@@ -1,8 +0,0 @@
-package org.wso2.carbon.automation.extensions.servers.tomcatserver;
-
-public enum TomcatServerType {
-    jaxrs,
-    jaxws,
-    webapp,
-    servelet
-}
