f636494afdf0dd890c4614059dcc0d7c632a13d0
warunap <warunap@wso2.com>
2014-05-26 13:49:41 +0530

    getcarbonhome bug fixed

--- test-automation-framework/org.wso2.carbon.automation.engine/src/main/java/org/wso2/carbon/automation/engine/frameworkutils/FrameworkPathUtil.java
+++ test-automation-framework/org.wso2.carbon.automation.engine/src/main/java/org/wso2/carbon/automation/engine/frameworkutils/FrameworkPathUtil.java
@@ -19,8 +19,10 @@ package org.wso2.carbon.automation.engine.frameworkutils;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.CarbonConstants;
 import org.wso2.carbon.automation.engine.FrameworkConstants;
 import org.wso2.carbon.automation.engine.frameworkutils.enums.OperatingSystems;
+import org.wso2.carbon.utils.ServerConstants;
 
 import java.io.File;
 
@@ -91,11 +93,11 @@ public class FrameworkPathUtil {
     }
 
     public static String getCarbonHome() {
-        if (System.getProperty("user.dir") != null) {
-            return System.getProperty("user.dir");
-        } else {
-            log.error("Cannot read carbon.home property ");
-            return null;
+        String carbonHome = System.getProperty(ServerConstants.CARBON_HOME);
+        if (carbonHome == null) {
+            carbonHome = System.getenv(CarbonConstants.CARBON_HOME_ENV);
+            System.setProperty(ServerConstants.CARBON_HOME, carbonHome);
         }
+        return carbonHome;
     }
 }
