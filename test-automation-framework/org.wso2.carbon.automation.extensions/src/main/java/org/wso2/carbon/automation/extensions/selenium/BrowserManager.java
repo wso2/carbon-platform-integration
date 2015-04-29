7672df40ce2c668fe8b6b8b72995d4a535aae454
warunap <warunap@wso2.com>
2015-04-29 09:22:00 +0530

    selenium issue fixed

--- test-automation-framework/org.wso2.carbon.automation.extensions/src/main/java/org/wso2/carbon/automation/extensions/selenium/BrowserManager.java
+++ test-automation-framework/org.wso2.carbon.automation.extensions/src/main/java/org/wso2/carbon/automation/extensions/selenium/BrowserManager.java
@@ -50,7 +50,11 @@ public class BrowserManager {
         if (automationContext.getConfigurationNode(String.format(XPathConstants.SELENIUM_REMOTE_WEB_DRIVER_URL))
                 .getAttributes().item(0).getNodeValue().equals("false")) {
 
-            if (System.getenv().containsKey("JOB_URL")) {
+            /*log.info("Test runs on " + driverSelection + " browser...");
+            getDriver(driverSelection);
+            driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+            return driver;*/
+            if (System.getenv().containsKey("SELENIUM_PLATFORM")) {
                 log.info("Test runs on Sauce Labs environment...");
                 log.info("Operating System : " + System.getenv().get("SELENIUM_PLATFORM") +
                         " On Browser " + System.getenv().get("SELENIUM_BROWSER") + " version "+
