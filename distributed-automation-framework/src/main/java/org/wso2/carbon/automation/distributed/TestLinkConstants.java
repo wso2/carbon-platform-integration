7c153ac994846512f42303de56682de5e1459774
Yasassri Ratnayake <ycrnet@gmail.com>
2016-11-07 09:53:20 +0530

    Removing redundant constants

--- distributed-automation-framework/src/main/java/org/wso2/carbon/automation/distributed/TestLinkConstants.java
+++ distributed-automation-framework/src/main/java/org/wso2/carbon/automation/distributed/TestLinkConstants.java
@@ -27,9 +27,7 @@ public class TestLinkConstants {
     public static final String TESTLINK_DEV_KEY = "devkey";
     public static final String TESTLINK_PROJECT = "project";
     public static final String TESTLINK_TESTPLAN = "testplan";
-    public static final String TESTLINK_DEFAULT_PLATFORM = "pt01";
     public static final String TESTLINK_CUSTOM_FIELD = "testlinkcustomfield";
-    public static final String TESTLINK_BUILD_NAME = "buildname";
     public static final String TESTLINK_Server_INFO = "serverinfo";
     public static final String TESTLINK_CONFIG_FILE_NAME = "test-link.yaml";
 }
