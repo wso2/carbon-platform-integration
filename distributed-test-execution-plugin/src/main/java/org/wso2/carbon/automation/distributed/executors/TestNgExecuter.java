14bb1c67c92084c6e70a646c12455fe047e604b0
Yasassri Ratnayake <ycrnet@gmail.com>
2017-05-12 17:46:21 +0530

    Refactoring code

--- distributed-test-execution-plugin/src/main/java/org/wso2/carbon/automation/distributed/executors/TestNgExecuter.java
+++ distributed-test-execution-plugin/src/main/java/org/wso2/carbon/automation/distributed/executors/TestNgExecuter.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.wso2.carbon.automation.distributed.executors;
 
 import org.apache.commons.logging.Log;
@@ -5,7 +20,6 @@ import org.apache.commons.logging.LogFactory;
 import org.testng.TestListenerAdapter;
 import org.testng.TestNG;
 import org.testng.TestNGException;
-import org.testng.annotations.BeforeSuite;
 import org.testng.xml.XmlClass;
 import org.testng.xml.XmlSuite;
 import org.testng.xml.XmlTest;
@@ -29,7 +43,7 @@ import java.util.List;
 public class TestNgExecuter {
     private static final Log log = LogFactory.getLog(TestNgExecuter.class);
 
-    @BeforeSuite public void executeEnvironment() throws IOException {
+    public void executeEnvironment() throws IOException {
         try {
             DistributedPlatformExtension my = new DistributedPlatformExtension();
             my.initiate();
